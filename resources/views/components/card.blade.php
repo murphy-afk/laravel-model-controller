bbcc5a473bfd28ab7574e5263043bd9b2806ec98
murphy-afk <bardarenoemi@gmail.com>
2026-04-08 12:47:40 +0200

    feat: stars

--- resources/views/components/card.blade.php
+++ resources/views/components/card.blade.php
@@ -1,4 +1,7 @@
 <div class="card shadow-sm border-0" style="max-width: 22rem;">
+    @php
+        $vote = (float) trim($vote->toHtml());
+    @endphp
     <div class="card-body">
         <h5 class="card-title fw-bold text-primary mb-1">{{ $title }}</h5>
         <h6 class="card-subtitle mb-3 text-muted">{{ $originalTitle }}</h6>
@@ -10,10 +13,15 @@
             <li class="mb-1">
                 <span class="fw-semibold">Release date:</span> {{ $releaseDate }}
             </li>
-            <li>
-                <span class="fw-semibold">Vote:</span>
-                <span class="badge bg-success">{{ $vote }}</span>
-            </li>
+            @for ($i = 0; $i <= 10; $i++)
+                @if ($i < floor($vote))
+                    <i class="bi bi-star-fill star"></i>
+                @elseif ($i < ceil($vote))
+                    <i class="bi bi-star-half star"></i>
+                @else
+                    <i class="bi bi-star star"></i>
+                @endif
+            @endfor
         </ul>
     </div>
 </div>
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -6,6 +6,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Document</title>
   @vite(['resources/sass/app.scss', 'resources/js/app.js'])
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.css">
 </head>
 <body>
   <x-header>My Movies Laravel</x-header>
